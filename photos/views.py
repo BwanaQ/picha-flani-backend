4cf90a6d497efa05fdfcc3878e1916a09a8a3991
BwanaQ <thunjawax@gmail.com>
2023-07-12 15:52:31 +0300

    add permission classes to photo views

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,16 @@
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class PhotoList(generics.ListCreateAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (AllowAny,)
 
 
 class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticated,)
